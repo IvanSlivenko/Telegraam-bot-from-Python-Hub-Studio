74c0ba3a6e2269f42bedc4be401cb9e4025dee9b
IvanSlivenko <ivan8822@ukr.net>
2024-03-26 17:01:09 +0300

    26-03-2024-17-01

--- app_2.py
+++ app_2.py
@@ -9,18 +9,21 @@ bot = Bot(token = TOKEN)
 dp = Dispatcher()
 #===========================================
 
-current_name = 'Шановний клієнт'
+# current_name = 'Шановний клієнт'
 
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer("Ви активували команду 'Start'")
+    await message.answer(f"{message.from_user.full_name} - Ви активували команду 'Start'")
 
 @dp.message()# обробник подій без фільтрів розміщуємо після всіх інших обробників подій
-async def echo(message: types.Message):
-
-        await message.answer(f"{current_name},\n ваша фраза : '{message.text}' нам не зрозуміла. \n  Спробуйте короткі фрази на зразок: {GRETING_PHRASES} чи {FAREWELL_PHRASES}")
+async def echo(message: types.Message, bot:Bot):
+        # await bot.send_message(message.from_user.id, 'Відповідь')
+        # await message.answer(f"{message.from_user.full_name} - ,\n ваша фраза : '{message.text}' нам не зрозуміла. \n  Спробуйте короткі фрази на зразок: {GRETING_PHRASES} чи {FAREWELL_PHRASES}")
+        await message.answer(message.text)
+        await message.reply(message.text)
 #--------------------------------------------------------------------
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 asyncio.run(main())
--- rawapitest(https).py
+++ rawapitest(https).py
@@ -4,19 +4,20 @@ from const import TOKEN, URL_TELEGRAM
 
 url = f"{URL_TELEGRAM}{TOKEN}/"
 
-async  def send_message(chat_id, text):
+
+async def send_message(chat_id, text):
     async  with aiohttp.ClientSession() as session:
-        params = {'chat_id': chat_id, 'text' : text }
-        async with session.post(url + 'send_message',data = params) as response:
+        params = {'chat_id': chat_id, 'text': text}
+        async with session.post(url + 'sendMessage',data=params) as response:
             await response.json()
 
 async def handle_updates(update):
     message = update.get('message', False)
     if message:
         chat_id = message['chat']['id']
-        text  = message.get('text', False)
+        text = message.get('text', False)
         if text:
-            await  send_message(chat_id,f'Ехо{text}')
+            await send_message(chat_id,f'Ехо:{text}')
         else:
             await send_message(chat_id,'Я працюю лише з текстом')
 
@@ -25,16 +26,17 @@ async def get_updates():
     async with aiohttp.ClientSession() as session:
         while True:
             params = {'timeout': 10, 'offset': offset}
-            async with session.post(url + 'get_updates', data = params) as response:
+            async with session.post(url + 'getUpdates', data=params) as response:
                 updates = await response.json()
                 if len(updates['result']) > 0:
                     offset = updates['result'][-1]['update_id'] + 1
                     for update in updates['result']:
                         await handle_updates(update)
 
-                        for_print = update.copy()
-                        for_print['message']['from']['id'] = 1234567890
-                        for_print['message']['chat']['id'] = 9987654321
+                        # for_print = update.copy()
+                        # for_print['message']['from']['id'] = 1234567890
+                        # for_print['message']['chat']['id'] = 9987654321
+                        print(update)
 
 async def main():
     await get_updates()
