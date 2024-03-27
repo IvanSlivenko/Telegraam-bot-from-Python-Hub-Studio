c6f538b439e25d08d98148470c5281dc7dcdad50
IvanSlivenko <ivan8822@ukr.net>
2024-03-27 17:13:30 +0300

    27-03-2024-17-13

--- app_3.py
+++ app_3.py
@@ -1,9 +1,9 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
 from dotenv import find_dotenv, load_dotenv
-import os
 
 load_dotenv(find_dotenv())
 
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,11 +1,19 @@
 from aiogram import types, Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+from const import COMMANDS_LIST
 
 user_private_router = Router()
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer(f"{message.from_user.full_name} - Ви активували команду 'Start'")
+    await message.answer(f"Доброго дня {message.from_user.full_name} - я віртуальний помічник ")
 
+@user_private_router.message(Command('menu'))
+async def menu_comands(message: types.Message):
+        await message.answer('Тут буде меню')
+
+@user_private_router.message(Command('cl'))
+async def commands_list(message: types.Message):
+        await message.answer(COMMANDS_LIST)
 @user_private_router.message()# обробник подій без фільтрів розміщуємо після всіх інших обробників подій
 async def echo(message: types.Message):
         await message.answer(message.text)
